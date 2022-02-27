05b90c1fcf3c63c1019c7f365478849fbef0c44f
Benn Day <benn.day@p5th.co>
2022-02-28 07:02:27 +1000

    update store

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,7 @@ Vue.use(Vuex);
 export default new Vuex.Store({
 	state: function () {
 		return {
-			allDogs: null,
+			allDogs: [],
 			selectedDogs: [],
 			numberSelected: null,
 			selectedItems: [],
